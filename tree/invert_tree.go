7a864e45d4d9f23355115d8eef8f93a2b529a743
Patrick T Coakley <contact@patricktcoakley.com>
2022-09-11 17:43:25 -0400

    Update solutions

--- tree/invert_tree.go
+++ tree/invert_tree.go
@@ -9,3 +9,25 @@ func invertTree(root *TreeNode) *TreeNode {
 
 	return root
 }
+
+func invertTreeIterative(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	q := []*TreeNode{root}
+
+	for len(q) > 0 {
+		head := q[0]
+		q = q[1:]
+		head.Left, head.Right = head.Right, head.Left
+		if head.Left != nil {
+			q = append(q, head.Left)
+		}
+
+		if head.Right != nil {
+			q = append(q, head.Right)
+		}
+	}
+	return root
+}
--- tree/invert_tree_test.go
+++ tree/invert_tree_test.go
@@ -32,3 +32,30 @@ func TestInvertTree(t *testing.T) {
 		}
 	}
 }
+
+func TestInvertTreeIterative(t *testing.T) {
+	tests := []struct {
+		root *TreeNode
+		want *TreeNode
+	}{
+		{
+			root: createTree([]*int{intPtr(4), intPtr(2), intPtr(7), intPtr(1), intPtr(3), intPtr(6), intPtr(9)}),
+			want: createTree([]*int{intPtr(4), intPtr(7), intPtr(2), intPtr(9), intPtr(6), intPtr(3), intPtr(1)}),
+		},
+		{
+			root: createTree([]*int{intPtr(2), intPtr(1), intPtr(3)}),
+			want: createTree([]*int{intPtr(2), intPtr(3), intPtr(1)}),
+		},
+		{
+			root: nil,
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		got := invertTreeIterative(tt.root)
+		if !cmp.Equal(tt.want, got) {
+			t.Errorf("want %v, got %v, diff %s", tt.want, got, cmp.Diff(tt.want, got))
+		}
+	}
+}
